27b428b55469e182859d8ace45a64539daa2be07
plan <plan@plan-tp>
2016-03-04 12:01:42 +0800

    Change README and config.py comment

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 icmp-monitor
 ============
 
-monitor network using icmp.
+Monitor network using icmp.
 
-single thread parallel ping, with custom alarm level for each ip, use SMTP to send alarm mail.
+Single thread parallel ping, with custom alarm level for each ip.
+Use SMTP to send alarm mail.
+Supports monitor target specified by hostname or IP.
+
+Configuration example
+---------------------
+Refer to config.py
+
+Log files
+----------
+icmp_mon.log    # start / stop message
+
+alarm.log       #link up/down alarm
+
+link.log        # regular log for monitor target, with rtt average
+
--- config.py
+++ config.py
@@ -12,15 +12,15 @@ log_backup_count = 10
 log_level = "DEBUG"
 # for log.py
 
-log_length_per_link = 60
-alarm_levels = [10, 60, 600]
-recover = 20
+log_length_per_link = 60       # Link state will be put into 60 size bitmap, 1 for up, 0 for down
+alarm_levels = [10, 60, 600]   # Send level 1/2/3 alarm after 10/60/600 seconds down time
+recover = 20   # Recover after 20 seconds no packet loss
 links = {
-    "127.0.0.1": None,
-        "192.168.1.1": {
-            "alarm_levels": [10, 60, 600],
-            "recover": 20,
-        },
+    "127.0.0.1": None,  # Use default settings
+    "192.168.1.1": {    # Override default settings
+        "alarm_levels": [10, 60, 600],
+        "recover": 20,
+    },
 }
 
 
